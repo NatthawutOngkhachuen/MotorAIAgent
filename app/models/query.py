6347ee5bd3481fa6122b457b8313d7cbcd202b76
NatthawutOngkhachuen <natthawutongkhachuen@gmail.com>
2026-04-10 02:58:14 +0700

    Delete app directory

--- app/models/query.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Literal
-
-class SearchRequest(BaseModel):
-    keyword: str
-
-class ChatRequest(BaseModel):
-    question: str
-    language: Literal["th", "en"] = "th"
